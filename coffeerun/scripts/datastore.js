cb66d849e822f01790ea47764714b74ef28f938c
Fausto Pérez <faustoperez@gmail.com>
2017-05-25 09:54:50 +0200

    Chapter 14 exercises - Coffeerun complete

--- coffeerun/scripts/datastore.js
+++ coffeerun/scripts/datastore.js
@@ -1,25 +1,34 @@
 (function (window) {
   'use strict';
   var App = window.App || {};
+  var Promise = window.Promise;
 
   function DataStore() {
     this.data = {};
   }
 
+  function promiseResolveWidth(value) {
+    var promise = new Promise(function (resolve, reject) {
+      resolve(value);
+    });
+    return promise;
+  }
+
   DataStore.prototype.add = function (key, val) {
-    this.data[key] = val;
+    return promiseResolveWidth(null);
   };
 
   DataStore.prototype.get = function (key) {
-    return this.data[key];
+    return promiseResolveWidth(this.data[key]);
   };
 
   DataStore.prototype.getAll = function () {
-    return this.data;
+    return promiseResolveWidth(this.data);
   }
 
   DataStore.prototype.remove = function (key) {
     delete this.data[key];
+    return promiseResolveWidth(null);
   }
 
   App.DataStore = DataStore;
